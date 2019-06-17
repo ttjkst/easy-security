03d9ab6e902439dc8cf3ba620d9c216f5d0053dc
ttjkst <791599901@qq.com>
2019-06-18 07:21:05 +0800

    c

--- server-defualt-ui/src/main.js
+++ server-defualt-ui/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import store from '@/components/store/store.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 Vue.config.productionTip = false
-
+Vue.use(Vuex)
 new Vue({
+  el:"#app",
+  store,
   render: h => h(App),
-}).$mount('#app')
+});
